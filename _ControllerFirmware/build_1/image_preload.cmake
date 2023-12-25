9c48d940aee3b32ba2dd1c6228c033ec88711a4b
ebertmx <87283949+ebertmx@users.noreply.github.com>
2023-12-25 14:54:00 -0800

    deleted build directory

--- _ControllerFirmware/build_1/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dongle_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/users/ebert/onedrive/projects/dodgeballclock/_controllerfirmware;c:/users/ebert/onedrive/projects/dodgeballclock/_displayfirmware;C:/ncs/v2.4.2/nrf;C:/ncs/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/ebert/OneDrive/Projects/DodgeballClock/_ControllerFirmware/prj.conf;c:/Users/ebert/OneDrive/Projects/DodgeballClock/_ControllerFirmware/boards/nrf52840dongle_nrf52840.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/ebert/OneDrive/Projects/DodgeballClock/_ControllerFirmware/boards/nrf52840dongle_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
