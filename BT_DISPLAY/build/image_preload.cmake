c8d84ebfad08556b85b11babd03a21941e2216a3
ebertmx <87283949+ebertmx@users.noreply.github.com>
2023-12-25 14:33:45 -0800

    Fixed folder and file organization

--- BT_DISPLAY/build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dongle_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/Users/ebert/Downloads/BLE-Remote-main/BLE-Remote-main/_Firmware/BT_DISPLAY/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
